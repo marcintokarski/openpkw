b324af100a3a45c705df7fc89d6849881593dbdf
Sebastian Celejewski <sebastian.celejewski@wp.pl>
2015-03-07 16:21:05 +0100

    Tworzenie PDFa przy pomocy oryginalnego formularza

---
+++ poc/src/webapp/src/main/java/pl/openpkw/poc/webapp/Formularz.java
@@ -0,0 +1,272 @@
+package pl.openpkw.poc.webapp;
+
+
+public class Formularz {
+
+	@FormElement(page = 1, x = 200, y = 750)
+	private String kodTerytorialnyGminy;
+	
+	@FormElement(page = 1, x = 480, y = 750)
+	private String numerObwoduGlosowania;
+	
+	@FormElement(page = 1, x = 220, y = 720)
+	private String siedzibaObwodowejKomisjiWyborczej;
+	
+	@FormElement(page = 1, x = 110, y = 695)
+	private String gminaDzielnica;
+	
+	@FormElement(page = 1, x = 250, y = 695)
+	private String powiat;
+	
+	@FormElement(page = 1, x = 440, y = 695)
+	private String wojewodztwo;
+	
+	@FormElement(page = 1, x = 290, y = 645)
+	private String dataGlosowania;
+	
+	@FormElement(page = 1, x = 220, y = 625)
+	private String dzienMiesiacGlosowania;
+
+	@FormElement(page = 1, x = 305, y = 625)
+	private String rokGlosowania;
+
+	@FormElement(page = 1, x = 365, y = 625)
+	private String godzinaGlosowaniaOd;
+	
+	@FormElement(page = 1, x = 460, y = 625)
+	private String godzinaGlosowaniaDo;
+	
+	@FormElement(page = 1, x = 420, y = 585)
+	private String pole_I_1;
+	
+	@FormElement(page = 1, x = 420, y = 535)
+	private String pole_I_2;
+	
+	@FormElement(page = 1, x = 420, y = 505)
+	private String pole_I_3;
+	
+	@FormElement(page = 1, x = 420, y = 475)
+	private String pole_I_4;
+	
+	@FormElement(page = 1, x = 420, y = 425)
+	private String pole_I_5;
+	
+	@FormElement(page = 1, x = 420, y = 368)
+	private String pole_I_6;
+	
+	@FormElement(page = 1, x = 420, y = 310)
+	private String pole_I_7;
+	
+	@FormElement(page = 1, x = 420, y = 280)
+	private String pole_I_8;
+	
+	@FormElement(page = 1, x = 420, y = 251)
+	private String pole_I_8a;
+	
+	@FormElement(page = 1, x = 420, y = 222)
+	private String pole_I_8b;
+	
+	@FormElement(page = 1, x = 420, y = 193)
+	private String pole_I_8c;
+	
+	@FormElement(page = 1, x = 420, y = 164)
+	private String pole_I_8d;
+	
+	@FormElement(page = 1, x = 420, y = 135)
+	private String pole_I_8e;
+
+	public String getKodTerytorialnyGminy() {
+		return kodTerytorialnyGminy;
+	}
+
+	public void setKodTerytorialnyGminy(String kodTerytorialnyGminy) {
+		this.kodTerytorialnyGminy = kodTerytorialnyGminy;
+	}
+
+	public String getNumerObwoduGlosowania() {
+		return numerObwoduGlosowania;
+	}
+
+	public void setNumerObwoduGlosowania(String numerObwoduGlosowania) {
+		this.numerObwoduGlosowania = numerObwoduGlosowania;
+	}
+
+	public String getSiedzibaObwodowejKomisjiWyborczej() {
+		return siedzibaObwodowejKomisjiWyborczej;
+	}
+
+	public void setSiedzibaObwodowejKomisjiWyborczej(String siedzibaObwodowejKomisjiWyborczej) {
+		this.siedzibaObwodowejKomisjiWyborczej = siedzibaObwodowejKomisjiWyborczej;
+	}
+
+	public String getGminaDzielnica() {
+		return gminaDzielnica;
+	}
+
+	public void setGminaDzielnica(String gminaDzielnica) {
+		this.gminaDzielnica = gminaDzielnica;
+	}
+
+	public String getPowiat() {
+		return powiat;
+	}
+
+	public void setPowiat(String powiat) {
+		this.powiat = powiat;
+	}
+
+	public String getWojewodztwo() {
+		return wojewodztwo;
+	}
+
+	public void setWojewodztwo(String wojewodztwo) {
+		this.wojewodztwo = wojewodztwo;
+	}
+
+	public String getDataGlosowania() {
+		return dataGlosowania;
+	}
+
+	public void setDataGlosowania(String dataGlosowania) {
+		this.dataGlosowania = dataGlosowania;
+	}
+
+	public String getGodzinaGlosowaniaOd() {
+		return godzinaGlosowaniaOd;
+	}
+
+	public void setGodzinaGlosowaniaOd(String godzinaGlosowaniaOd) {
+		this.godzinaGlosowaniaOd = godzinaGlosowaniaOd;
+	}
+
+	public String getGodzinaGlosowaniaDo() {
+		return godzinaGlosowaniaDo;
+	}
+
+	public void setGodzinaGlosowaniaDo(String godzinaGlosowaniaDo) {
+		this.godzinaGlosowaniaDo = godzinaGlosowaniaDo;
+	}
+
+	public String getPole_I_1() {
+		return pole_I_1;
+	}
+
+	public void setPole_I_1(String pole_I_1) {
+		this.pole_I_1 = pole_I_1;
+	}
+
+	public String getPole_I_2() {
+		return pole_I_2;
+	}
+
+	public void setPole_I_2(String pole_I_2) {
+		this.pole_I_2 = pole_I_2;
+	}
+
+	public String getPole_I_3() {
+		return pole_I_3;
+	}
+
+	public void setPole_I_3(String pole_I_3) {
+		this.pole_I_3 = pole_I_3;
+	}
+
+	public String getPole_I_4() {
+		return pole_I_4;
+	}
+
+	public void setPole_I_4(String pole_I_4) {
+		this.pole_I_4 = pole_I_4;
+	}
+
+	public String getPole_I_5() {
+		return pole_I_5;
+	}
+
+	public void setPole_I_5(String pole_I_5) {
+		this.pole_I_5 = pole_I_5;
+	}
+
+	public String getPole_I_6() {
+		return pole_I_6;
+	}
+
+	public void setPole_I_6(String pole_I_6) {
+		this.pole_I_6 = pole_I_6;
+	}
+
+	public String getPole_I_7() {
+		return pole_I_7;
+	}
+
+	public void setPole_I_7(String pole_I_7) {
+		this.pole_I_7 = pole_I_7;
+	}
+
+	public String getPole_I_8() {
+		return pole_I_8;
+	}
+
+	public void setPole_I_8(String pole_I_8) {
+		this.pole_I_8 = pole_I_8;
+	}
+
+	public String getPole_I_8a() {
+		return pole_I_8a;
+	}
+
+	public void setPole_I_8a(String pole_I_8a) {
+		this.pole_I_8a = pole_I_8a;
+	}
+
+	public String getPole_I_8b() {
+		return pole_I_8b;
+	}
+
+	public void setPole_I_8b(String pole_I_8b) {
+		this.pole_I_8b = pole_I_8b;
+	}
+
+	public String getPole_I_8c() {
+		return pole_I_8c;
+	}
+
+	public void setPole_I_8c(String pole_I_8c) {
+		this.pole_I_8c = pole_I_8c;
+	}
+
+	public String getPole_I_8d() {
+		return pole_I_8d;
+	}
+
+	public void setPole_I_8d(String pole_I_8d) {
+		this.pole_I_8d = pole_I_8d;
+	}
+
+	public String getPole_I_8e() {
+		return pole_I_8e;
+	}
+
+	public void setPole_I_8e(String pole_I_8e) {
+		this.pole_I_8e = pole_I_8e;
+	}
+
+	public String getDzienMiesiacGlosowania() {
+		return dzienMiesiacGlosowania;
+	}
+
+	public void setDzienMiesiacGlosowania(String dzienMiesiacGlosowania) {
+		this.dzienMiesiacGlosowania = dzienMiesiacGlosowania;
+	}
+
+	public String getRokGlosowania() {
+		return rokGlosowania;
+	}
+
+	public void setRokGlosowania(String rokGlosowania) {
+		this.rokGlosowania = rokGlosowania;
+	}
+
+	
+	
+}
--- poc/src/webapp/src/main/java/pl/openpkw/poc/webapp/PdfGenerator.java
+++ poc/src/webapp/src/main/java/pl/openpkw/poc/webapp/PdfGenerator.java
@@ -1,8 +1,20 @@
 package pl.openpkw.poc.webapp;
 
 import java.io.ByteArrayOutputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
 
 import org.xhtmlrenderer.pdf.ITextRenderer;
+
+import com.lowagie.text.Element;
+import com.lowagie.text.Font;
+import com.lowagie.text.Image;
+import com.lowagie.text.Phrase;
+import com.lowagie.text.pdf.BaseFont;
+import com.lowagie.text.pdf.ColumnText;
+import com.lowagie.text.pdf.PdfContentByte;
+import com.lowagie.text.pdf.PdfReader;
+import com.lowagie.text.pdf.PdfStamper;
 
 public class PdfGenerator {
 
@@ -23,5 +35,47 @@ public class PdfGenerator {
 		} catch (Exception ex) {
 			throw new RuntimeException("Failed to generate a PDF document: " + ex.getMessage(), ex);
 		}
+	}
+
+	public byte[] stamp(Formularz formularz) {
+		try {
+			PdfReader pdfReader = new PdfReader(this.getClass().getResource("/templates/prezydent_2015.pdf"));
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			PdfStamper pdfStamper = new PdfStamper(pdfReader, out);
+
+			Field[] fields = Formularz.class.getDeclaredFields();
+			for (Field f : fields) {
+				f.setAccessible(true);
+				Annotation a = f.getAnnotation(FormElement.class);
+				String text = (String) f.get(formularz);
+				int page = ((FormElement) a).page();
+				int x = ((FormElement) a).x();
+				int y = ((FormElement) a).y();
+				print(pdfStamper, text, page, x, y);
+			}
+			
+			for (int i = 1; i <= pdfReader.getNumberOfPages(); i++) {
+				Image barcode = Image.getInstance(this.getClass().getResource("/barcode.jpeg"));
+				barcode.setAbsolutePosition(45f,  10f);
+				PdfContentByte content = pdfStamper.getUnderContent(i);
+				content.addImage(barcode);
+			}
+
+			pdfStamper.close();
+			out.close();
+			return out.toByteArray();
+		} catch (Exception ex) {
+			throw new RuntimeException("Failed to stam the form: " + ex.getMessage(), ex);
+		}
+
+	}
+
+	private void print(PdfStamper stamper, String text, int page, int x, int y) throws Exception {
+		PdfContentByte content = stamper.getUnderContent(page);
+		BaseFont bf = BaseFont.createFont("/fonts/arial.ttf", BaseFont.IDENTITY_H, BaseFont.EMBEDDED);
+		Font f = new Font(bf, 10);
+		f.setColor(255, 0, 0);
+		Phrase p = new Phrase(text, f);
+		ColumnText.showTextAligned(content, Element.ALIGN_LEFT, p, x, y, 0);
 	}
 }
